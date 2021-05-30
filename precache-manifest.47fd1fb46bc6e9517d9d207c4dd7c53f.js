3cca8d881ba47208301ce37e24d621fc31d74a2e
IsaLegal <li.forero@uniandes.edu.co>
2021-05-30 00:44:51 -0500

    Updates

---
+++ precache-manifest.47fd1fb46bc6e9517d9d207c4dd7c53f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6aa0c09013dbfce948c8cfb7a86a89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a7f4b356148eddd1e46",
+    "url": "/static/css/main.37a0d4b3.chunk.css"
+  },
+  {
+    "revision": "7b9854439c18ed8cb84c",
+    "url": "/static/js/2.80b0831b.chunk.js"
+  },
+  {
+    "revision": "3a7f4b356148eddd1e46",
+    "url": "/static/js/main.6a0592a2.chunk.js"
+  },
+  {
+    "revision": "87071df12e1cadbcce35",
+    "url": "/static/js/runtime~main.b2cfdd2c.js"
+  }
+]);
